85d83498e1b2cba5b77cc781ee5892e3a453719a
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-03-10 00:06:10 +0530

    Dashboard

---
+++ backend/app/routes/sql_lab.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from app.database import get_db
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class SQLQuery(BaseModel):
+    query: str
+
+
+@router.post("/sql-lab/run")
+def run_query(payload: SQLQuery, db: Session = Depends(get_db)):
+
+    try:
+        result = db.execute(text(payload.query))
+
+        rows = result.fetchall()
+        columns = result.keys()
+
+        data = [dict(zip(columns, row)) for row in rows]
+
+        return {
+            "columns": columns,
+            "rows": data
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from app.routes import sql_lab
 from fastapi import FastAPI
 from app.database import engine
 from app import models
@@ -10,3 +11,4 @@ app = FastAPI()
 
 # include routes
 app.include_router(dashboard.router)
+app.include_router(sql_lab.router)
