ad8b7b091b1a7683ee73651326a666cce3563312
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-07-13 23:17:04 +0530

    Backend  github

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -13,3 +13,17 @@ class DashboardStatsResponse(DashboardStatsBase):
 
     class Config:
         orm_mode = True
+
+class ContributionDay(BaseModel):
+    date: str
+    contribution_count: int
+    contribution_level: str
+
+
+class ContributionWeek(BaseModel):
+    contribution_days: list[ContributionDay]
+
+
+class GithubContributionResponse(BaseModel):
+    total_contributions: int
+    weeks: list[ContributionWeek]
